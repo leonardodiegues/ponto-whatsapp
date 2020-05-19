84693a9fd0ed8556899b930c57480ed1f291b6cd
Leonardo Diegues <dscleo@gmail.com>
2020-05-18 22:14:45 -0300

    add logo to navbar (to be deprecated)

---
+++ www/plugins/logo.js
@@ -0,0 +1,3 @@
+var header = $('.navbar > .container-fluid');
+
+header.append('<div style=\"float:right\"><a href=\"https://github.com/leonardodiegues/ponto-whatsapp\"><img src=\"images/GitHub-Mark-Light-32px.png\" alt=\"alt\" style=\"float:right;width:33px;height:41px;padding-top:10px;\" class=\"github-logo\"></a></div>');
